55a3c791f8d2711fa9bf00f1d220c9007ec9bf51
SebasAy <sebasaa2002@gmail.com>
2023-09-30 08:54:28 -0500

    Inicializacion

---
+++ Parcial2_Scripting/Parcial2_Scripting/Bike.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_Scripting
+{
+    internal class Bike
+    {
+        public float speed;
+        public float aceleration;
+        public float handling;
+        public float grip;
+        public Bike() { }
+    }
+}
